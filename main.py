66f8a11937e4e2ab086bd81f6e087dc6e1225de0
Ramon <ramon_moratto@hotmail.com>
2025-10-17 22:01:54 -0300

    Gunicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Importação das bibliotecas
 from flask import Flask
+from tarefa import buscar_tarefas
 
 # Cria o objeto do flask
 app = Flask(__name__)
@@ -8,6 +9,12 @@ app = Flask(__name__)
 @app.route('/api')
 def index():
     return 'Api rodando'
+
+#Criando a rota que retorna as tarefas
+@app.route('/api/tarefas')
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 # Identifica que é o arquivo principal e liga o servidor executando o flask 😊
 if __name__== "__main__":
---
+++ tarefa.py
@@ -0,0 +1,14 @@
+def buscar_tarefas():
+    tarefas = [
+        {
+            "id": 1,
+            "tarefa": "Aprender HTML",
+            "status": "Pendente",
+        },
+        {
+            "id": 2,
+            "tarefa": "Aprender HTML",
+            "status": "Concluido",
+        } 
+    ]
+    return tarefas
